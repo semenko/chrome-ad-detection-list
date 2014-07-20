2d4eaf5a11b18a1f9a229431a42d91e5f597b53c
Nick Semenkovich <semenko@alum.mit.edu>
2014-07-20 14:27:11 -0500

    Update with uBlock third party lists, concatenated.
    367 unmapped of 539 total (32% mapped)

--- hashcollide.py
+++ hashcollide.py
@@ -9,10 +9,19 @@ def update_map(filename):
             if line.startswith("127.0.0.1") or line.startswith("0.0.0.0"):
                 hostname = line.split()[1]
                 map[sha256(hostname.strip()).hexdigest()[:16].upper()] = hostname  # TODO: Catch duplicates from collisions?
+                # Cut subdomains
+                periodsplit = hostname.split('.')
+                for i in range(1, len(periodsplit) - 1):
+                    subname = '.'.join(periodsplit[i:])
+                    map[sha256(subname.strip()).hexdigest()[:16].upper()] = subname
+                    
 
 update_map('someonewhocares.hosts')
 update_map('mvps.hosts')
 update_map('hosts-file.net')
+update_map('ublock-thirdparty.txt')
+
+print('Total map size: %d' % (len(map)))
 
 google_hashes = []
 with open('hashed_ad_networks.cc', 'r') as file:
